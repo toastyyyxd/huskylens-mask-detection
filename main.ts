8fee875866f9252226e5740e187f3dc2adf26e73
toasty <89341766+toastyyyxd@users.noreply.github.com>
2022-12-23 00:05:12 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+let lastMs = 0
+basic.forever(function () {
+    huskylens.request()
+    const box = huskylens.readBox_ss(1, Content3.ID)
+    const ms = control.millis()
+    if (ms > lastMs+2500) {
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+    }
+    if (box <= 0) return
+    if (box % 2 != 0) {
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        lastMs = ms
+    } else {
+        pins.digitalWritePin(DigitalPin.P13, 0)
+    }
+    if (box % 2 == 0) {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        lastMs = ms
+    } else {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+    }
+})
