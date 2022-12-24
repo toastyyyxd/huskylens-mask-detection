b756fee91095e758aa46181b390ae9619239791f
toasty <89341766+toastyyyxd@users.noreply.github.com>
2022-12-24 21:01:43 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,58 @@
+function showLedsPlot (pattern: string) {
+    basic.clearScreen()
+    pattern = pattern.replaceAll(' ', '')
+    const rows = pattern.split('\n').slice(1, -1)
+    for (let y = 0; y < rows.length; y++) {
+        const row = rows[y]
+        for (let x = 0; x < row.length; x++) {
+            if (row.charAt(x) == "#") {
+                led.plot(x, y)
+            }
+        }
+    }
+}
+let lastMs = 0
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
-let lastMs = 0
 basic.forever(function () {
     huskylens.request()
     const box = huskylens.readBox_ss(1, Content3.ID)
     const ms = control.millis()
-    if (ms > lastMs+2500) {
-        pins.digitalWritePin(DigitalPin.P13, 0) // red led
-        pins.digitalWritePin(DigitalPin.P14, 0) // green led
+    if (ms > lastMs + 1500) {
+        // red led
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        // green led
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        music.stopAllSounds()
+        basic.clearScreen()
     }
-    if (box <= 0) return
+    if (box <= 0 || !huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        return
+    }
     if (box % 2 != 0) {
         pins.digitalWritePin(DigitalPin.P13, 1)
+        music.ringTone(Note.C)
+        showLedsPlot(`
+# . . . #
+. # . # .
+. . # . .
+. # . # .
+# . . . #
+`)
         lastMs = ms
     } else {
         pins.digitalWritePin(DigitalPin.P13, 0)
+        music.stopAllSounds()
     }
     if (box % 2 == 0) {
         pins.digitalWritePin(DigitalPin.P14, 1)
+        showLedsPlot(`
+. . . . .
+. . . . #
+. . . # .
+# . # . .
+. # . . .
+`)
         lastMs = ms
     } else {
         pins.digitalWritePin(DigitalPin.P14, 0)
